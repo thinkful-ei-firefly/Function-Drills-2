4e463a96ce524e08a3255c4cd7d443773200e8b1
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-07-08 15:04:23 -0500

    To infinity done

---
+++ beyond.js
@@ -0,0 +1,19 @@
+function beyond(num) {
+    if (num === Infinity || num === -Infinity) {
+        console.log("And beyond")
+    }
+    else if (num > 0) {
+        console.log("To infinity")
+    }
+    else if (num < 0) {
+        console.log("To negative infinity"())
+    }
+    else if (num === 0) {
+        console.log("Staying home")
+    }
+    else {
+        console.log("Something happened")
+    }
+}
+
+beyond();
